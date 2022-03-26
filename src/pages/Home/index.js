4127fc46ea44ee27e32efaae7c2122dd7ea5fb75
artist <18855051882@163.com>
2022-03-27 02:37:44 +0800

    feat: 配置antd

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from 'antd';
+
+function Home() {
+   return(
+       <div>
+         <Button type="primary">这是一个主页</Button>
+       </div>
+   )
+ }
+export default Home;
